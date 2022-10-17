6b359c4ca7ba424a33bcc369973b3fb2dbcf313d
Firas <firas.bouadila@esprit.tn>
2022-10-17 20:23:51 +0100

    Init project

---
+++ All microService/salary-management-service/src/main/java/com/ms/salarymanagementservice/SalaryManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ms.salarymanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalaryManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalaryManagementServiceApplication.class, args);
+	}
+
+}
