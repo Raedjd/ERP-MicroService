e48986a4269e47058244246c74d9f338b1b028d9
Raedjd <raed.jaidi@esprit.tn>
2022-10-18 15:05:41 +0100

    In progress 🔧 🔨

--- All microService/Event-service/src/main/java/com/ms/eventservice/EventServiceApplication.java
+++ All microService/Event-service/src/main/java/com/ms/eventservice/EventServiceApplication.java
@@ -3,13 +3,23 @@ package com.ms.eventservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RestController
 public class EventServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EventServiceApplication.class, args);
     }
 
+    @GetMapping("/c")
+    @ResponseBody
+    public String r(){
+        return  "raed";
+    }
 }
