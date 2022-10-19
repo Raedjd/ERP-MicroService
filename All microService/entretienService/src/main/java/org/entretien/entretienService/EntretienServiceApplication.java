c9661441ce4c29ebf8d8ca1681484113a75d2709
Raedjd <raed.jaidi@esprit.tn>
2022-10-19 22:23:27 +0100

    In progress 🔧 👷

--- All microService/entretienService/src/main/java/org/entretien/entretienService/EntretienServiceApplication.java
+++ All microService/entretienService/src/main/java/org/entretien/entretienService/EntretienServiceApplication.java
@@ -2,8 +2,10 @@ package org.entretien.entretienService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class EntretienServiceApplication {
 
 	public static void main(String[] args) {
